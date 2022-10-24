75ddcbff4b27ed1f36f6cc7043eada4ff1bac7cc
Brooke Heck <b19heck@gmail.com>
2022-10-24 18:09:23 +0000

    change methods on form to a select

--- src/components/form/UrlForm.jsx
+++ src/components/form/UrlForm.jsx
@@ -5,25 +5,21 @@ import './UrlForm.scss';
 function UrlForm() {
   return (
     <Form id='urlForm'>
-      <Form.Group controlId="formURL">
-        <Form.Label>URL: </Form.Label>
-        <Form.Control type="text" placeholder="Enter URL" />
-      </Form.Group>
 
-      <Container id='methodsDiv'>
-        <Button className='method'>
-          GET
-        </Button>
-        <Button className='method' >
-          POST
-        </Button>
-        <Button className='method' >
-          PUT
-        </Button>
-        <Button className='method' >
-          DELETE
-        </Button>
+      <Container id='inputsContainer'>
+        <Form.Select>
+          <option value="get">GET</option>
+          <option value="post">POST</option>
+          <option value="put">PUT</option>
+          <option value="delete">DELETE</option>
+        </Form.Select>
+
+        <Form.Group controlId="formURL">
+          <Form.Label>URL: </Form.Label>
+          <Form.Control type="text" placeholder="Enter URL" />
+        </Form.Group>
       </Container>
+
 
       <Button type="submit" className='submitForm' >
         Submit
